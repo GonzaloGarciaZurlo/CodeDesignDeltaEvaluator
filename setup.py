a3186b6574157ef684c430910805cf133e83b444
GonzaloGarciaZurlo <gonzalogarciazurlo@gmail.com>
2024-09-16 04:20:10 -0300

    agrego setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="delta-design-evaluator",  
+    version="0.1.0",  
+    author="Gonzalo García Zurlo",
+    author_email="gonzalo.garcia.zurlo@mi.unc.edu.ar",
+    description="A system for evaluating implicit design changes in code during agile reviews",
+    url="https://github.com/GonzaloGarciaZurlo/CodeDesignDeltaEvaluator",  
+    packages=find_packages(), 
+    python_requires='>=3.7', 
+    install_requires=[
+        "networkx",  
+        "pyparsing",  
+        "pytest",     
+        "pylint",
+        "re"
+    ],
+)
