eaed0bfc89ffb19e5bf072ec2375f57994675288
GonzaloGarciaZurlo <gonzalogarciazurlo@gmail.com>
2025-09-22 11:12:37 -0300

    Examples changes

--- src/cdde/example.py
+++ src/cdde/example.py
@@ -1,30 +1,3 @@
 from abc import ABC, abstractmethod
 from typing import override
 
-
-class Example(ABC):
-    @abstractmethod
-    def method(self):
-        pass
-
-    @abstractmethod
-    def another_method(self):
-        pass
-
-    @abstractmethod
-    def yet_another_method(self):
-        pass
-
-
-class AnotherExample(Example):
-    @override
-    def method(self):
-        pass
-
-    @override
-    def another_method(self):
-        pass
-
-    @override
-    def yet_another_method(self):
-        pass
